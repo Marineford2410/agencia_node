4d431edf9534c1b07d054a78d0f1926f798998e6
Marineford2410 <marconava2410@gmail.com>
2021-03-06 01:43:28 -0600

    Config la conexion a la db con sequelize

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 import express from 'express';
 import router from './routes/index.js';
+import db from './config/db.js';
 
 const app = express();
+
+// Conectar a la base de datos
+db.authenticate()
+   .then( () => console.log('Base de Datos conectada'))
+   .catch( error => console.log(error));
+
 const port = process.env.PORT || 4000;    // Definicion del puerto
 
-app.use('/', router);                     // Estableciendo rutas
 app.set('view engine', 'pug');            // Habilitando PUG
+app.use( (request, response, next) => {
+   const year = new Date();
+   response.locals.actualYear = year.getFullYear();
+   response.locals.nombreSitio = 'Agencia de Viajes';
+   next();
+});
 app.use(express.static('public'));        // Definir la carpeta publica
+app.use('/', router);                     // Estableciendo rutas
 
 app.listen(port, () => {
    console.log(`El servidor esta funcionando en el puerto ${port}`);
