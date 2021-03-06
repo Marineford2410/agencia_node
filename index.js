c99e6f1cf2bb48db20e04c416e33c8846e154ef2
Marineford2410 <marconava2410@gmail.com>
2021-03-05 19:07:58 -0600

    Creando rutas

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 import express from 'express';
+import router from './routes/index.js';
 
 const app = express();
 const port = process.env.PORT || 4000;    // Definicion del puerto
 
-app.get('/', (request, response) => {
-   response.send('Hola Mundo');
-});
+app.use('/', router);
 
 app.listen(port, () => {
    console.log(`El servidor esta funcionando en el puerto ${port}`);
