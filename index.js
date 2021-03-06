d8d6645fa18c728de1516935cb6343ad93069f59
Marineford2410 <marconava2410@gmail.com>
2021-03-05 18:56:07 -0600

    Cambiando a Modules

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-const express = require('express');
+import express from 'express';
+
 const app = express();
 const port = process.env.PORT || 4000;    // Definicion del puerto
+
+app.get('/', (request, response) => {
+   response.send('Hola Mundo');
+});
 
 app.listen(port, () => {
    console.log(`El servidor esta funcionando en el puerto ${port}`);
