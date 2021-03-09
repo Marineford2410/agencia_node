42ce5ceee39a7da7947df96dda09ddf6782470ac
Marineford2410 <marconava2410@gmail.com>
2021-03-08 22:50:49 -0600

    Preparando para deployment

--- config/db.js
+++ config/db.js
@@ -1,8 +1,11 @@
 import Sequelize from 'sequelize';
+import { config } from 'dotenv';
 
-const db = new Sequelize('agenciaviajes', 'root', 'Diego2410', {
-   host: '127.0.0.1',
-   port: '3306',
+config({ path : 'variables.env' });
+
+const db = new Sequelize(process.env.BD_NOMBRE, process.env.BD_USER, process.env.BD_PASS, {
+   host: process.env.BD_HOST,
+   port: process.env.BD_PORT,
    dialect: 'mysql',
    define: {
       timestamps: false
--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 import express from 'express';
 import router from './routes/index.js';
 import db from './config/db.js';
+import { config } from 'dotenv';
+
 
 const app = express();
+config({ path : 'variables.env' });
 
 // Conectar a la base de datos
 db.authenticate()
    .then( () => console.log('Base de Datos conectada'))
    .catch( error => console.log(error));
 
+const host = process.env.HOST || '0.0.0.0';    // Definicion del puerto
 const port = process.env.PORT || 4000;    // Definicion del puerto
 
 app.set('view engine', 'pug');            // Habilitando PUG
@@ -25,7 +29,7 @@ app.use(express.urlencoded({ extended: true }));   // Agregar body parser para l
 app.use(express.static('public'));        // Definir la carpeta publica
 app.use('/', router);                     // Estableciendo rutas
 
-app.listen(port, () => {
+app.listen(port, host, () => {
    console.log(`El servidor esta funcionando en el puerto ${port}`);
 })
 
